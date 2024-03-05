b51c3d2e3bf8b42449a494b2d6f429a56728d716
Mubarek Seid Juhar <mubareksd@gmail.com>
2024-03-05 09:28:53 +0300

    :sparkles: feat: sync

---
+++ gms/gms/web_form/rate_trainer/rate_trainer.js
@@ -0,0 +1,9 @@
+frappe.ready(function() {
+    var member = {{ frappe.form_dict.member | json }}
+
+    frappe.web_form.set_value('member', member);
+
+    frappe.web_form.on('member', (field, value) => {
+	    frappe.web_form.set_df_property('member', 'hidden', 1);
+    })
+})
